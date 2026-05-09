0fdf88cd48ba7675e84bfd4097bc5d8298dab182
Nadden Auguste Laventure <96151359+nalav816@users.noreply.github.com>
2026-05-09 18:42:22 -0400

    scale now manipulated through media query in css not javascript

--- src/App.js
+++ src/App.js
@@ -11,24 +11,10 @@ function App() {
   const [isMobile, setIsMobile] = useState();
  
   useEffect(() => {
-     //Dyanmically scales images by integer values within the entire App 
-    function updateScale() {
-      const style = getComputedStyle(document.documentElement)
-      const baseWidth = parseFloat(style.getPropertyValue('--baseWidth'));
-      //Multiplied by a factor of 1.5 so that the window makes sure there is enough space for sections larger than base height before
-      //Scaling components up
-      const screenWidth = window.innerWidth;
-
-      //Represents the minimum amount of padding accepted
-      const sidePadding = window.innerWidth * .03;
-      let multiplier = Math.max(1, Math.floor((screenWidth - (sidePadding * 2))/baseWidth));
-      
-      document.documentElement.style.setProperty('--scale', Math.min(2, multiplier));
-      setScale(Math.min(2, multiplier));
-    }
-
     const handleResize = () => {
-      updateScale();
+      const style = getComputedStyle(document.documentElement)
+     
+      setScale(style.getPropertyValue('--scale'));
       setIsMobile(window.innerWidth <= 1024);
     }
     
